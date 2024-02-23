1966c272a494aa69f38d0970ebc377e321c300ce
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2024-02-23 19:51:46 +0200

    Create _01_SortByNameAndAge

---
+++ _02_Encapsulation/_01_Lab/_01_SortByNameAndAge/Person.java
@@ -0,0 +1,29 @@
+package _02_Encapsulation._01_Lab._01_SortByNameAndAge;
+
+public class Person {
+    private final static String TO_STRING_TEMPLATE = "%s %s is %d years old.";
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format(TO_STRING_TEMPLATE,this.getFirstName(),this.lastName,this.getAge());
+    }
+
+}
