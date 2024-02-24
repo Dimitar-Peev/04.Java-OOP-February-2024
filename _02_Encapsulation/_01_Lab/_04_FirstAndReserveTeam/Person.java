e77d63d3736239b5eba5ba70ab6a3baa505356d7
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2024-02-24 18:47:35 +0200

    Create _04_FirstAndReserveTeam

---
+++ _02_Encapsulation/_01_Lab/_04_FirstAndReserveTeam/Person.java
@@ -0,0 +1,71 @@
+package _04_FirstAndReserveTeam;
+
+public class Person {
+    private final static String TO_STRING_TEMPLATE = "%s %s gets %f leva";
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setAge(age);
+        this.setSalary(salary);
+    }
+
+    public void setSalary(double salary) {
+        if (salary < 460) {
+            throw new IllegalArgumentException("Salary cannot be less than 460 leva");
+        }
+        this.salary = salary;
+    }
+
+    public void setAge(int age) {
+        if (age < 1) {
+            throw new IllegalArgumentException("Age cannot be zero or negative integer");
+        }
+        this.age = age;
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName.length() < 3) {
+            throw new IllegalArgumentException("Last name cannot be less than 3 symbols");
+        }
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName.length() < 3) {
+            throw new IllegalArgumentException("First name cannot be less than 3 symbols");
+        }
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(TO_STRING_TEMPLATE, this.getFirstName(), this.getLastName(), this.getSalary());
+    }
+
+    public void increaseSalary(double bonus) {
+        bonus = this.getAge() < 30 ? bonus / 2 : bonus;
+        this.setSalary(this.getSalary() + (this.getSalary() * bonus) / 100);
+    }
+
+}
---
+++ _02_Encapsulation/_01_Lab/_04_FirstAndReserveTeam/Team.java
@@ -0,0 +1,47 @@
+package _04_FirstAndReserveTeam;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Team {
+
+    private String name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+
+    public Team(String name) {
+        this(name, new ArrayList<>(), new ArrayList<>());
+    }
+
+    private Team(String name, List<Person> firstTeam, List<Person> reserveTeam) {
+        this.setName(name);
+        this.firstTeam = firstTeam;
+        this.reserveTeam = reserveTeam;
+    }
+
+    public void addPlayer(Person person) {
+        if (person.getAge() < 40) {
+            this.firstTeam.add(person);
+        } else {
+            this.reserveTeam.add(person);
+        }
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Person> getFirstTeam() {
+        return Collections.unmodifiableList(this.firstTeam);
+    }
+
+    public List<Person> getReserveTeam() {
+        return Collections.unmodifiableList(this.reserveTeam);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
