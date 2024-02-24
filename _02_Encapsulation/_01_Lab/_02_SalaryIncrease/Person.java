8f83e26eb112073210b7a37ebf2dbe12769ea352
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2024-02-24 16:37:06 +0200

    Create _02_SalaryIncrease

---
+++ _02_Encapsulation/_01_Lab/_02_SalaryIncrease/Person.java
@@ -0,0 +1,42 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double bonus) {
+        if (age < 30) {
+            bonus /= 2;
+        }
+
+        this.salary = this.salary + this.salary * bonus / 100;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s gets %f leva", firstName, lastName, salary);
+    }
+}
