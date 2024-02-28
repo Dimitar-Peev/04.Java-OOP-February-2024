d24f062b734f3ae97fc78bb7662f06f51159fe0a
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2024-02-28 20:08:55 +0200

    Create _03_RandomArrayList

---
+++ _03_Inheritance/_01_Lab/_03_RandomArrayList/RandomArrayList.java
@@ -0,0 +1,12 @@
+package _03_RandomArrayList;
+
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomArrayList<T> extends ArrayList<T> {
+
+    public T getRandomElement() {
+        int index = ThreadLocalRandom.current().nextInt(0, size());
+        return remove(index);
+    }
+}
