b93ebf3aafe0a254016cbfb713270fcab02480cc
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2024-02-29 17:53:28 +0200

    add package zoo

--- _03_Inheritance/_02_Exercise/_02_Zoo/zoo/Mammal.java
+++ _03_Inheritance/_02_Exercise/_02_Zoo/zoo/Mammal.java
@@ -1,4 +1,4 @@
-package _02_Zoo;
+package _03_Inheritance._02_Exercise._02_Zoo.zoo;
 
 public class Mammal extends Animal {
     public Mammal(String name) {
--- _03_Inheritance/_02_Exercise/_02_Zoo/zoo/Snake.java
+++ _03_Inheritance/_02_Exercise/_02_Zoo/zoo/Snake.java
@@ -1,4 +1,4 @@
-package _02_Zoo;
+package _03_Inheritance._02_Exercise._02_Zoo.zoo;
 
 public class Snake extends Reptile {
     public Snake(String name) {
