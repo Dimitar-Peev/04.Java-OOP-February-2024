735aa1767472d38bc5c717889e27bc3d93cfb8e3
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2024-03-15 19:47:07 +0200

    Create _01_NumberInRange.java

---
+++ _08_ExceptionsAndErrorHandling/_01_Lab/_01_NumberInRange.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _01_NumberInRange {
+    private static int start;
+    private static int end;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int[] range = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        start = range[0];
+        end = range[1];
+
+        System.out.println(String.format("Range: [%d...%d]", start, end));
+
+        while (true) {
+            String input = scanner.nextLine();
+            try {
+                int num = Integer.parseInt(input);
+                if (isInRange(num)) {
+                    System.out.println("Valid number: " + num);
+                    return;
+                } else {
+                    System.out.println("Invalid number: " + input);
+                }
+            } catch (NumberFormatException ex) {
+                System.out.println("Invalid number: " + input);
+            }
+        }
+    }
+
+    private static boolean isInRange(int num) {
+        return start <= num && num <= end;
+    }
+}
