8df107d86c9eda2a37caa36cfab37a300ff2e4a9
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2024-03-17 11:44:15 +0200

    Update StateRepository.java

--- Java-OOP-Exams/15. Java OOP Retake Exam - 22 August 2021/glacialExpedition/repositories/StateRepository.java
+++ Java-OOP-Exams/15. Java OOP Retake Exam - 22 August 2021/glacialExpedition/repositories/StateRepository.java
@@ -8,29 +8,29 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class StateRepository implements Repository<State> {
-    private Map<String, State> explorers;
+    private Map<String, State> states;
 
     public StateRepository() {
-        this.explorers = new LinkedHashMap<>();
+        this.states = new LinkedHashMap<>();
     }
 
     @Override
     public Collection<State> getCollection() {
-        return Collections.unmodifiableCollection(this.explorers.values());
+        return Collections.unmodifiableCollection(this.states.values());
     }
 
     @Override
     public void add(State entity) {
-        this.explorers.put(entity.getName(), entity);
+        this.states.put(entity.getName(), entity);
     }
 
     @Override
     public boolean remove(State entity) {
-        return this.explorers.remove(entity.getName()) != null;
+        return this.states.remove(entity.getName()) != null;
     }
 
     @Override
     public State byName(String name) {
-        return this.explorers.get(name);
+        return this.states.get(name);
     }
 }
