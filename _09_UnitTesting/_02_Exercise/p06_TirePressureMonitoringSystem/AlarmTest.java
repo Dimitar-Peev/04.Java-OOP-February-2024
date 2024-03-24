0bb4736b9fbf5225b78bbdc8bdecf4a5784e6e5c
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2024-03-24 22:05:26 +0200

    Update AlarmTest.java

--- _09_UnitTesting/_02_Exercise/p06_TirePressureMonitoringSystem/AlarmTest.java
+++ _09_UnitTesting/_02_Exercise/p06_TirePressureMonitoringSystem/AlarmTest.java
@@ -31,48 +31,4 @@ public class AlarmTest {
         alarm.check();
         assertFalse(alarm.getAlarmOn());
     }
-
-
-
-     /*  Десислава Топузакова
-
-    //1. гума с нормално налягане: [17; 21]
-    @Test
-    public void testAlarmWithNormalValue () {
-        Sensor sensor = Mockito.mock(Sensor.class);
-        Alarm alarm = new Alarm(sensor);
-        //налагяне 19.1
-        Mockito.when(sensor.popNextPressurePsiValue()).thenReturn(19.3);
-        //проверка на налягането
-        alarm.check();
-        //нормално налягане -> false
-        Assert.assertFalse(alarm.getAlarmOn());
-    }
-
-    //2. гума с ниско налягане: < 17 -> алармата е включена (true)
-    @Test
-    public void testAlarmWithLowerValue() {
-        Sensor sensor = Mockito.mock(Sensor.class);
-        Alarm alarm = new Alarm(sensor);
-        //налагяне 15.3
-        Mockito.when(sensor.popNextPressurePsiValue()).thenReturn(15.3);
-        //проверка на налягането
-        alarm.check();
-        //по-ниско налягане -> true
-        Assert.assertTrue(alarm.getAlarmOn());
-    }
-
-    //3. гума с високо налягане: > 21 -> алармата е включена (true)
-    @Test
-    public void testAlarmWithHigherValue() {
-        Sensor sensor = Mockito.mock(Sensor.class);
-        Alarm alarm = new Alarm(sensor);
-        //налагяне 24.1
-        Mockito.when(sensor.popNextPressurePsiValue()).thenReturn(24.1);
-        //проверка на налягането
-        alarm.check();
-        //по-високо налягане -> true
-        Assert.assertTrue(alarm.getAlarmOn());
-    }
-     */
 }
