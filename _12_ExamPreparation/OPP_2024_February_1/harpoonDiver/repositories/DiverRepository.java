05047529933991f53057855e9bde5e0b8584ebb8
Dimitar-Peev <mctrix87@abv.bg>
2024-04-08 16:35:15 +0300

    Update DiverRepository.java

--- _12_ExamPreparation/OPP_2024_February_1/harpoonDiver/repositories/DiverRepository.java
+++ _12_ExamPreparation/OPP_2024_February_1/harpoonDiver/repositories/DiverRepository.java
@@ -5,46 +5,29 @@ import harpoonDiver.models.diver.Diver;
 import java.util.*;
 
 public class DiverRepository implements Repository<Diver> {
-    //  private Collection<Diver> divers;
-    private final Map<String, Diver> diverMap; //TODO checkname
+    private Collection<Diver> divers;
 
     public DiverRepository() {
-        //      this.divers = new ArrayList<>();
-        this.diverMap = new LinkedHashMap<>();
+        this.divers = new ArrayList<>();
     }
 
     @Override
     public Collection<Diver> getCollection() {
-        //  return Collections.unmodifiableCollection(this.divers);
-        return Collections.unmodifiableCollection(this.diverMap.values());
-
+        return Collections.unmodifiableCollection(this.divers);
     }
 
     @Override
     public void add(Diver diver) {
-        //     this.divers.add(diver);
-        this.diverMap.putIfAbsent(diver.getName(), diver);
+        this.divers.add(diver);
     }
 
     @Override
     public boolean remove(Diver diver) {
-//        return this.divers.remove(diver);
-        return this.diverMap.remove(diver.getName()) != null;
-//        if (divers.containsKey(diver.getName())) {
-//            divers.remove(diver);
-//            return true;
-//        }
-//        return false;
+        return this.divers.remove(diver);
     }
 
     @Override
     public Diver byName(String name) {
-//        return this.divers.stream().filter(d -> d.getName().equals(name)).findFirst().orElse(null);
-        return this.diverMap.get(name);
-
-//        if (divers.containsKey(name)) {
-//            return divers.get(name);
-//        }
-//        return null;
+        return this.divers.stream().filter(d -> d.getName().equals(name)).findFirst().orElse(null);
     }
 }
