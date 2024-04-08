9875ea8a42617ac7a50f714ca06dd2c3149e023d
Dimitar-Peev <mctrix87@abv.bg>
2024-04-08 16:33:25 +0300

    Update Main.java

--- _12_ExamPreparation/OPP_2024_February_1/harpoonDiver/Main.java
+++ _12_ExamPreparation/OPP_2024_February_1/harpoonDiver/Main.java
@@ -9,7 +9,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Иван Иванов - 29.03.2024
         Controller controller = new ControllerImpl();
         Engine engine = new EngineImpl(controller);
         engine.run();
