9791b95678f7bc3a70d9702b4faa2ccc36a4cffe
Dimitar-Peev <mctrix87@abv.bg>
2025-05-15 16:55:27 +0300

    Add "Work.java"

---
+++ Java-OOP-Exams/23. Java OOP Retake Exam - 16 April 2020/RobotService/src/robotService/models/procedures/Work.java
@@ -0,0 +1,22 @@
+package robotService.models.procedures;
+
+import robotService.models.robots.interfaces.Robot;
+
+public class Work extends BaseProcedure {
+
+    private static final int HAPPINESS = 12;
+
+    private static final int ENERGY = 6;
+
+    @Override
+    public void doService(Robot robot, int procedureTime) {
+
+        super.doService(robot, procedureTime);
+
+        int newHappiness = robot.getHappiness() + HAPPINESS;
+        robot.setHappiness(newHappiness);
+
+        int newEnergy = robot.getEnergy() - ENERGY;
+        robot.setEnergy(newEnergy);
+    }
+}
