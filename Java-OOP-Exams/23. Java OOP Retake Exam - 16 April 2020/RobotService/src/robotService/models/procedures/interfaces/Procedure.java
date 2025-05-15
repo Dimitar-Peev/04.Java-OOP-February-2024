6097de3df16734a9ef6370097af56811e4349a12
Dimitar-Peev <mctrix87@abv.bg>
2025-05-15 16:54:18 +0300

    Add "Procedure.java"

---
+++ Java-OOP-Exams/23. Java OOP Retake Exam - 16 April 2020/RobotService/src/robotService/models/procedures/interfaces/Procedure.java
@@ -0,0 +1,11 @@
+package robotService.models.procedures.interfaces;
+
+import robotService.models.robots.interfaces.Robot;
+
+public interface Procedure {
+
+    String history();
+
+    void doService(Robot robot, int procedureTime);
+
+}
