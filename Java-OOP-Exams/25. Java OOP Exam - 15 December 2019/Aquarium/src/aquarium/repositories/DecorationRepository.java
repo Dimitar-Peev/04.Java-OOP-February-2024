729f171fee5426beeb64b8603702d30fba6a7d0c
Dimitar-Peev <mctrix87@abv.bg>
2025-08-05 20:22:49 +0300

    Create DecorationRepository.java

---
+++ Java-OOP-Exams/25. Java OOP Exam - 15 December 2019/Aquarium/src/aquarium/repositories/DecorationRepository.java
@@ -0,0 +1,30 @@
+package aquarium.repositories;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import aquarium.models.decorations.Decoration;
+
+public class DecorationRepository implements Repository {
+
+    private Collection<Decoration> decorations;
+
+    public DecorationRepository() {
+        this.decorations = new ArrayList<>();
+    }
+
+    @Override
+    public void add(Decoration decoration) {
+        this.decorations.add(decoration);
+    }
+
+    @Override
+    public boolean remove(Decoration decoration) {
+        return this.decorations.remove(decoration);
+    }
+
+    @Override
+    public Decoration findByType(String type) {
+        return this.decorations.stream().filter(d -> d.getClass().getSimpleName().equals(type)).findFirst().orElse(null);
+    }
+}
