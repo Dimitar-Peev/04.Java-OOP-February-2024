934a615a89978a9ff0c3e89fd34aa652229235d8
Dimitar-Peev <mctrix87@abv.bg>
2025-08-19 20:20:55 +0300

    Create ExceptionMessages.java

---
+++ Java-OOP-Exams/28. Java OOP Retake Exam - 18 April 2019/PlayersAndMonsters/src/common/ExceptionMessages.java
@@ -0,0 +1,18 @@
+package common;
+
+public class ExceptionMessages {
+    public static final String PLAYER_NAME_NULL_OR_EMPTY = "Player's username cannot be null or an empty string.";
+    public static final String CARD_NAME_NULL_OR_EMPTY = "Card's name cannot be null or an empty string.";
+
+    public static final String PLAYER_HEALTH_LESS_THAN_ZERO = "Player's health bonus cannot be less than zero.";
+    public static final String CARD_HP_LESS_THAN_ZERO = "Card's HP cannot be less than zero.";
+    public static final String DAMAGE_LESS_THAN_ZERO = "Damage points cannot be less than zero.";
+    public static final String CARD_DAMAGE_LESS_THAN_ZERO = "Card's damage points cannot be less than zero.";
+
+    public static final String PLAYER_DEAD = "Player is dead!";
+    public static final String PLAYER_EXIST = "Player %s already exists!";
+    public static final String PLAYER_NULL = "Player cannot be null";
+
+    public static final String CARD_EXIST = "Card %s already exists!";
+    public static final String CARD_NULL = "Card cannot be null!";
+}
