bf18fd33157164122c210f4264810f6161e75cca
Dimitar-Peev <mctrix87@abv.bg>
2025-08-26 17:35:55 +0300

    Create SportsCar.java

---
+++ Java-OOP-Exams/21. Java OOP Retake Exam - 22 August 2020/easterRaces/entities/cars/SportsCar.java
@@ -0,0 +1,22 @@
+package easterRaces.entities.cars;
+
+
+import static easterRaces.common.ExceptionMessages.INVALID_HORSE_POWER;
+
+public class SportsCar extends BaseCar {
+    private static final double CUBIC_CENTIMETERS = 3000;
+    private static final double HORSEPOWER_MIX = 250;
+    private static final double HORSEPOWER_MAX = 450;
+
+    public SportsCar(String model, int horsePower) {
+        super(model, horsePower, CUBIC_CENTIMETERS);
+    }
+
+    @Override
+    protected void setHorsePower(int horsePower) {
+        if (horsePower < HORSEPOWER_MIX || horsePower > HORSEPOWER_MAX) {
+            throw new IllegalArgumentException(String.format(INVALID_HORSE_POWER, horsePower));
+        }
+        super.setHorsePower(horsePower);
+    }
+}
