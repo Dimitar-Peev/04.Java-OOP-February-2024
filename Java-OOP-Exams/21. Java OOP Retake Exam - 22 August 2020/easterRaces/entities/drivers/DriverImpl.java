6e65b58093462ea22482a4d10425770f208d148e
Dimitar-Peev <mctrix87@abv.bg>
2025-08-27 19:57:05 +0300

    Create DriverImpl.java

---
+++ Java-OOP-Exams/21. Java OOP Retake Exam - 22 August 2020/easterRaces/entities/drivers/DriverImpl.java
@@ -0,0 +1,69 @@
+package easterRaces.entities.drivers;
+
+import easterRaces.entities.cars.Car;
+
+import static easterRaces.common.ExceptionMessages.CAR_INVALID;
+import static easterRaces.common.ExceptionMessages.INVALID_NAME;
+
+public class DriverImpl implements Driver {
+    private String name;
+    private Car car;
+    private int numberOfWins;
+    private boolean canParticipate;
+
+    public DriverImpl(String name) {
+        this.setName(name);
+        this.car = null;
+        this.numberOfWins = 0;
+        this.canParticipate = false;
+    }
+
+    private void setName(String name) {
+        if (name == null || name.isBlank() || name.length() < 5) {
+            throw new IllegalArgumentException(String.format(INVALID_NAME, name, 5));
+        }
+        this.name = name;
+    }
+
+    private void setCar(Car car) {
+        this.car = car;
+    }
+
+    private void setCanParticipate() {
+        this.canParticipate = true;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public Car getCar() {
+        return this.car;
+    }
+
+    @Override
+    public int getNumberOfWins() {
+        return this.numberOfWins;
+    }
+
+    @Override
+    public void addCar(Car car) {
+        if (car == null) {
+            throw new IllegalArgumentException(CAR_INVALID);
+        }
+        this.setCar(car);
+        this.setCanParticipate();
+    }
+
+    @Override
+    public void winRace() {
+        this.numberOfWins += 1;
+    }
+
+    @Override
+    public boolean getCanParticipate() {
+        return this.canParticipate;
+    }
+}
