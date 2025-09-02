4931aac34cb41e1b73fd8776b74d0c2549fba9b0
Dimitar-Peev <mctrix87@abv.bg>
2025-09-02 06:44:55 +0300

    Create ConstantMessages.java

---
+++ Java-OOP-Exams/29. Java OOP Advanced Retake Exam - 12 May 2017/Cresla/src/cresla/common/ConstantMessages.java
@@ -0,0 +1,17 @@
+package cresla.common;
+
+public class ConstantMessages {
+    public static final String REACTOR_CREATED = "Created %s Reactor - %d";
+
+    public static final String MODULE_ADDED = "Added %s - %d to Reactor - %d";
+
+    public static final String REACTOR_INFO = """
+            %s - %s
+            Energy Output: %s
+            Heat Absorbing: %s
+            Modules: %s""";
+
+    public static final String MODULE_INFO = "%s Module - %d%n";
+    public static final String ENERGY_INFO = "Energy Output: %d";
+    public static final String ABSORBER_INFO = "Heat Absorbing: %d";
+}
